138ec7aa4b10310c4ac5820cd7ce8ad494aafe61
Calum Bell <calumabell@googlemail.com>
2021-11-29 17:23:08 +0000

    added prismic integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,21 @@
+require('dotenv').config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "hesperos",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+        customTypesApiToken: process.env.PRISMIC_CUSTOM_TYPES_API_TOKEN,
+      },
+    }
+  ],
 };
