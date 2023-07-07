2450b269305b19d4a4ebf46df35739d77f2c1b8a
calumbell <calumabell@googlemail.com>
2023-07-07 17:48:10 +0100

    fixed off by one error in BuyTicketLink cmpnt

--- src/components/BuyTicketLink/index.jsx
+++ src/components/BuyTicketLink/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { ExternalLink } from '../';
+import React from "react";
+import { ExternalLink } from "../";
 
 const BuyTicketLink = ({ event }) => {
-  const isPast = Date.parse(event.date) < Date.now()
-  if (isPast) return (
-    <p className='uppercase text-primary'>This event has passed</p>
-  )
+  const isPast = Date.now() > Date.parse(event.date);
+  if (isPast)
+    return <p className="uppercase text-primary">This event has passed</p>;
 
-
-  return <ExternalLink
-    url={event.buy_ticket_link.url}
-    title='Buy Tickets'
-    altTitle='No advance tickets available'
-  />
-}
+  return (
+    <ExternalLink
+      url={event.buy_ticket_link.url}
+      title="Buy Tickets"
+      altTitle="No advance tickets available"
+    />
+  );
+};
 
 export default BuyTicketLink;
