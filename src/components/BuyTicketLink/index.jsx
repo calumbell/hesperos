c63a1b02206d054fac960795b684098fa2f879d4
calumbell <calumabell@googlemail.com>
2023-07-08 08:19:16 +0100

    fixed further bugs with buy ticket link

--- src/components/BuyTicketLink/index.jsx
+++ src/components/BuyTicketLink/index.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import { ExternalLink } from "../";
 
 const BuyTicketLink = ({ event }) => {
-  const isPast = Date.now() > Date.parse(event.date);
-  if (isPast)
+  const today = new Date();
+  const concertDate = new Date(event.date).setHours(
+    event.time.text.split(":")[0]
+  );
+
+  if (today > concertDate)
     return <p className="uppercase text-primary">This event has passed</p>;
 
   return (
