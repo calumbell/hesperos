70d6c3975cd5f2a186aa9352e43f104bc9b3621a
calumbell <calumabell@googlemail.com>
2024-07-02 22:26:48 +0100

    refactored Card cmpnt using tailwind

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import * as styles from './Card.module.scss';
 
 const Card = ({ data }) => {
   return (
     <Link 
       to={`/${data.subroute}/${data.uid}`} 
-      className={`${styles.card} p-0 hover-shadow border-faded`}
+      className="rounded bg-light relative p-0 hover:shadow border-faded"
     >
       <GatsbyImage 
-        className={`${styles.cardImage} mb-2 block`}
+        className="h-96 sm:h-[68%] mb-2 block"
         image={data.image}
         alt={data.altText ?? ''}
       />
@@ -18,22 +17,23 @@ const Card = ({ data }) => {
         <time className='mb-0 font-extralight'>{data.date}</time>
 
         <h2 className={`font-serif
-          ${data.title.length >= 20 && 'text-xl'}`} // resize long txt
+          ${data.title.length < 20 ? 'text-2xl' : 'text-xl'}`} // resize long txt
         >
           {data.title}
         </h2>
 
         { data.subtitle &&
-          <sub className={`text-faded font-light font-sans letter-spacing-3 
-            ${data.subtitle.length <= 25 ? `text-sm` : `text-xs`}`}
-          > {data.subtitle}</sub>
+          <sub className={`text-faded font-light text-primary font-sans letter-spacing-3 
+            ${data.subtitle.length < 25 ? `text-sm` : `text-xs`}
+          `}>
+            {data.subtitle}
+          </sub>
         }
       </div>
 
       {data.displayDateBubble &&
         <div 
-          className={`p-2 bg-light text-center ${styles.dateBubble}`}
-          style={{'--gap': '0rem'}}
+          className="absolute flex flex-col items-center rounded-full p-2 size-20 bg-light/70 top-1 right-1 text-center"
         >
           <p className='p-0 mt-1 text-3xl'>{data.date.split(" ")[1]}</p>
           <p className='p-0 m-0 2xl'>{data.date.split(" ")[2].slice(0,3)}</p>
